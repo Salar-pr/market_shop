c95de95a6d087e4f384502cd4652eb592bee1838
salar <salar_pr@outlook.com>
2025-01-15 15:09:01 +0330

    add item in database and fix bog

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import database
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,9 +98,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
             # }
 
 # my_database
-import importlib
-database = importlib.import_module('database')
-DATABASES = database.DATABASES
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
